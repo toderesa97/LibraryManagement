5948a2dee15be1f01bc6e784a96b78b4b68595db
Thomas DRS <toderesa97@gmail.com>
2016-11-23 18:43:14 +0000

    Update Album.java

--- src/Album.java
+++ src/Album.java
@@ -40,8 +40,8 @@ public class Album {
     }
     @Override
     public boolean equals(Object obj){
-        return ((Album)obj).canciones.size() == canciones.size() && (obj instanceof Album) ? 
-                canciones.stream().map(p->p.equals(((Album)obj).
-                canciones.stream().map(u->u))).count() == canciones.size() : false;
+        return ((Album)obj).canciones.size() == canciones.size() && (obj instanceof Album) 
+                && canciones.stream().map(p->p.equals(((Album)obj).
+                canciones.stream().map(u->u))).count() == canciones.size();
     }
 }
